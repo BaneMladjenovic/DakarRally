7c8c3295f909d8b258de2ef4e91d153f88d4200b
Branislav Mladjenovic <branislav.mladjenovic@rationaletech.com>
2021-02-07 09:36:51 +0100

    Feature #0001 Core Project Functionality
    Setup and implementation of project core functionalities that includes: configuration of in-memory db (sqlite), creating and commencing racing simulations as well as various task requirements.

---
+++ DakarRally.Repository/Bootstrap/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using DakarRally.Repository.Interfaces;
+using DakarRally.Repository.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DakarRally.Repository.Bootstrap
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddDataAccess(this IServiceCollection service)
+        {
+            service.AddTransient<IRaceRepository, RaceRepository>();
+            service.AddTransient<IVehicleRepository, VehicleRepository>();
+        }
+    }
+}
---
+++ DakarRally.Repository/Interfaces/IVehicleRepository.cs
@@ -0,0 +1,19 @@
+﻿using DakarRally.Repository.Models;
+using DakarRally.Shared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DakarRally.Repository.Interfaces
+{
+    public interface IVehicleRepository
+    {
+        Task<IEnumerable<Vehicle>> GetVehiclesAsync();
+        Task<Vehicle> GetVehicleByIdAsync(int id);
+        Task PostVehicleAsync(Vehicle vehicle);
+        Task PutVehicleAsync(int id, Vehicle vehicle);
+        Task DeleteVehicleAsync(int id);
+        Task<IEnumerable<Vehicle>> FindVehiclesAsync(VehicleFilterDTO vehicleFilter);
+    }
+}
